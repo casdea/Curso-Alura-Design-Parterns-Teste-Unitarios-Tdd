70f1858ffc32076aee7b7c43f9953dc703e3d7fc
casdea <casdea@gmail.com>
2021-10-03 19:49:21 -0300

    BDD e Java - Introducao

---
+++ bdd-cucumber/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -0,0 +1,20 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PropondoLanceSteps {
+
+	@Given("Dado um lance valido")
+	public void dado_um_lance_valido() {
+	}
+
+	@When("Quando propoe o lance")
+	public void quando_propoe_o_lance() {
+	}
+	@Then("Entao o lance eh aceito")
+	public void entao_o_lance_eh_aceito() {
+	}
+
+}
